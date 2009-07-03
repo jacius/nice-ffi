rubygame/ruby-sdl-ffi

Added Rakefile with gem task.

Rakefile
@@ -1,0 +1,45 @@
+#--
+#
+# This file is one part of:
+#
+# FFI-SDL - Ruby-FFI bindings to SDL
+#
+# Copyright (c) 2009 John Croisant
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#++
+
+
+require 'rake'
+
+
+#############
+##   GEM   ##
+#############
+
+require 'rake/gempackagetask'
+
+# Load ffi-sdl.gemspec, which defines $gemspec.
+load File.join( File.dirname(__FILE__), "ffi-sdl.gemspec" )
+
+Rake::GemPackageTask.new( $gemspec ) do |pkg|
+  pkg.need_tar_bz2 = true
+end

jacius/nice-ffi

Rakefile
@@ -1,0 +1,45 @@
+#--
+#
+# This file is one part of:
+#
+# Nice-FFI - Convenience layer atop Ruby-FFI
+#
+# Copyright (c) 2009 John Croisant
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#++
+
+
+require 'rake'
+
+
+#############
+##   GEM   ##
+#############
+
+require 'rake/gempackagetask'
+
+# Load nice-ffi.gemspec, which defines $gemspec.
+load File.join( File.dirname(__FILE__), "nice-ffi.gemspec" )
+
+Rake::GemPackageTask.new( $gemspec ) do |pkg|
+  pkg.need_tar_bz2 = true
+end