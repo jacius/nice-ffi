21ea3187775cd646c4ddb6c9d3e8b83805835d09
John Croisant <jacius@gmail.com>
2009-07-27 16:03:49 -0500

    Revamped OpaqueStruct.
    Not subclass of Struct anymore. Supports memory autorelease.

--- lib/nice-ffi/opaquestruct.rb
+++ lib/nice-ffi/opaquestruct.rb
@@ -37,40 +37,92 @@
 # only use methods provided by the C library to allocate, modify,
 # or free the struct's memory.
 #
-class NiceFFI::OpaqueStruct < NiceFFI::Struct
+# OpaqueStruct supports the same memory autorelease system as
+# NiceStruct. Define MyClass.release( pointer ) to call the library
+# function to free the struct, and pass an FFI::Pointer to #new. You
+# can disable autorelease for an individual instance by providing
+# {:autorelease => false} as an option to #new
+#
+class NiceFFI::OpaqueStruct
+
+  class << self
+
+    # Returns a NiceFFI::TypedPointer instance for this class.
+    def typed_pointer
+      @typed_pointer or
+        (@typed_pointer = NiceFFI::TypedPointer.new(self))
+    end
 
 
-  # Create a new instance of the struct, wrapping an FFI::Pointer.
-  # You can pass another instance of this class to create a new
-  # instance wrapping the same pointer.
-  #
-  def initialize( val )
+    # Calls the class's release method if it exists. Used for autorelease.
+    def _release( pointer )       # :nodoc:
+      if( respond_to?(:release) )
+        release( pointer )
+      end
+    end
+
+  end
+
+
+  # Create a new instance of the class, wrapping (not copying!) a
+  # FFI::Pointer. You can pass another instance of this class to
+  # create a new instance wrapping the same pointer.
+  # 
+  # If val is an instance of FFI::Pointer and you have defined
+  # MyClass.release, the pointer will be passed to MyClass.release 
+  # when the instance is garbage collected. Use MyClass.release to
+  # free the memory for the struct, as appropriate for your class.
+  # To disable autorelease for this instance, set {:autorelease => false}
+  # in +options+.
+  # 
+  # (Note: FFI::MemoryPointer and FFI::Buffer have built-in memory
+  # management, so MyClass.release is never called for them.)
+  # 
+  def initialize( val, options={} )
+    options = {:autorelease => true}.merge!( options )
+
     case val
-    when FFI::Pointer
-      send(:pointer=, val)
+
     when self.class
-      send(:pointer=, val.pointer)
+      @pointer = initialize( val.pointer, options )
+
+    when FFI::AutoPointer
+      @pointer = val
+
+    when FFI::NullPointer
+      @pointer = val
+
+    when FFI::Pointer
+      if( val.instance_of? FFI::Pointer ) # not MemoryPointer or Buffer
+        if( self.class.respond_to?(:release) and options[:autorelease] )
+          # Wrap in an AutoPointer to call self.class._release when it's GC'd.
+          @pointer = FFI::AutoPointer.new( val, self.class.method(:_release) )
+        else
+          @pointer = val
+        end
+      else
+        raise TypeError, "unsupported pointer type #{val.class.name}"
+      end
+
+
     else
       raise TypeError, "cannot create new #{self.class} from #{val.inspect}"
+
     end
   end
 
 
-  # Raises ArgumentError. An OpaqueStruct has no members.
-  def []( key )
-    raise ArgumentError, "No such field '#{key}'"
+  attr_reader :pointer
+
+  def to_ptr
+    @pointer
   end
 
 
-  # Raises ArgumentError. An OpaqueStruct has no members.
-  def []=( key, value )
-    raise ArgumentError, "No such field '#{key}'"
+  def to_s
+    "#<%s:%#.x>"%[self.class.name, self.object_id]
   end
 
-
-  # Returns []. An OpaqueStruct has no members.
-  def members
-    []
-  end
+  alias :inspect :to_s
 
 end
