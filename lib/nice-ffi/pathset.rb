c9b91904209de2e69480d12170a63cf26ae44410
John Croisant <jacius@gmail.com>
2010-01-30 02:52:27 -0600

    Fixed PathSet failing when a path doesn't end with a separator.

--- lib/nice-ffi/pathset.rb
+++ lib/nice-ffi/pathset.rb
@@ -465,8 +465,8 @@ class NiceFFI::PathSet
     results = paths.collect do |path|
       files.collect do |file|
         names.collect do |name|
-          # Concat path and file, fill in for [NAME], and expand.
-          File.expand_path( (path+file).gsub("[NAME]", name) )
+          # Join path and file, fill in for [NAME], and expand.
+          File.expand_path( File.join(path,file).gsub("[NAME]",name) )
         end
       end
     end
